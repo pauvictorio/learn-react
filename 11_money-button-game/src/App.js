2a369e98c9148c40792f310a1781fb8c6401ae46
Paulo Victorio <paulo_victorio@yahoo.com>
2023-04-09 22:47:53 +0800

    Add betting functions

--- 11_money-button-game/src/App.js
+++ 11_money-button-game/src/App.js
@@ -1,9 +1,53 @@
-import React, { useState, useEffect } from 'react'
-import './assets/css/App.css'
-import Game from './components/Game'
+import React, { useState } from 'react'
+import './assets/css/App.css';
+import Game from './components/Game';
+import MessageBoard from './components/MessageBoard';
 
 export default function App() {
+	const [balance, setBalance] = useState(500);
+	const [chance, setChance] = useState(10);
+
+	const gameInfo = {
+		low: {
+			id: 'low',
+			name: 'Low Risk',
+			min: -25,
+			max: 100
+		},
+		moderate: {
+			id: 'moderate',
+			name: 'Moderate Risk',
+			min: -100,
+			max: 1000
+		},
+		high: {
+			id: 'high',
+			name: 'High Risk',
+			min: -500,
+			max: 2500
+		},
+		severe: {
+			id: 'severe',
+			name: 'Severe Risk',
+			min: -3000,
+			max: 5000
+		}
+	}
+
+	const randomizeBet = (min, max) => {
+		return Math.floor(Math.random() * (max - min + 1) + min);
+	}
+
+	const handleBet = (e) => {
+		const risk = e.target.id;
+		const bet = randomizeBet(gameInfo[risk].min, gameInfo[risk].max);
+		console.log(bet);
+	}
+
 	return (
-		<Game />
+		<>
+			<Game balance={balance} chance={chance} gameInfo={gameInfo} onBetClick={handleBet} />
+			<MessageBoard />
+		</>
 	)
 }

b77c2697af89900a8c911f0ec300a6a866dad51c
Paulo Victorio <paulo_victorio@yahoo.com>
2023-04-09 23:05:01 +0800

    Separate rendering of Game logic to its own function

--- 11_money-button-game/src/components/Game.jsx
+++ 11_money-button-game/src/components/Game.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
 import '../assets/css/Game.css';
+import MessageBoard from './MessageBoard';
+
+function renderGame(gameInfo, handleBet) {
+	return Object.keys(gameInfo).map((game) => {
+		return (
+			<div className="Game__buttonsContainer__buttonGroup" key={game}>
+				<h3>{gameInfo[game].name}</h3>
+				<button id={gameInfo[game].id} onClick={handleBet}>Bet</button>
+				<p>{gameInfo[game].min} to {gameInfo[game].max}</p>
+			</div>
+		)
+	})
+}
 
 export default function Game({ balance, chance, gameInfo, onBetClick }) {
 	const handleBet = (e) => {
@@ -7,25 +20,19 @@ export default function Game({ balance, chance, gameInfo, onBetClick }) {
 	}
 
 	return (
-		<div className="Game">
-			<div className="Game__userInfo">
-				<h1 className="Game__userInfo__balance">Your Money: {balance}</h1>
-				<h1 className="Game__userInfo__chance">Chance/s left: {chance}</h1>
+		<>
+			<div className="Game">
+				<div className="Game__userInfo">
+					<h1 className="Game__userInfo__balance">Your Money: {balance}</h1>
+					<h1 className="Game__userInfo__chance">Chance/s left: {chance}</h1>
+				</div>
+
+				<div className="Game__buttonsContainer">
+					{renderGame(gameInfo, handleBet)}
+				</div>
 			</div>
 
-			<div className="Game__buttonsContainer">
-				{
-					Object.keys(gameInfo).map((game) => {
-						return (
-							<div className="Game__buttonsContainer__buttonGroup" key={game}>
-								<h3>{gameInfo[game].name}</h3>
-								<button id={gameInfo[game].id} onClick={handleBet}>Bet</button>
-								<p>{gameInfo[game].min} to {gameInfo[game].max}</p>
-							</div>
-						)
-					})
-				}
-			</div>
-		</div>
+			<MessageBoard balance={balance} chance={chance} />
+		</>
 	);
 }

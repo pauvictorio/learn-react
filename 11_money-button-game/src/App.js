83b366441879a0dfd23cf096f8e4916065a7dc2d
Paulo Victorio <paulo_victorio@yahoo.com>
2023-04-10 00:40:10 +0800

    Organize code structure

--- 11_money-button-game/src/App.js
+++ 11_money-button-game/src/App.js
@@ -2,36 +2,38 @@ import React, { useState } from 'react'
 import './assets/css/App.css';
 import Game from './components/Game';
 
+const gameInfo = {
+	low: {
+		id: 'low',
+		name: 'Low Risk',
+		min: -25,
+		max: 100
+	},
+	moderate: {
+		id: 'moderate',
+		name: 'Moderate Risk',
+		min: -100,
+		max: 1000
+	},
+	high: {
+		id: 'high',
+		name: 'High Risk',
+		min: -500,
+		max: 2500
+	},
+	severe: {
+		id: 'severe',
+		name: 'Severe Risk',
+		min: -3000,
+		max: 5000
+	}
+}
+
 export default function App() {
 	const [balance, setBalance] = useState(500);
 	const [chance, setChance] = useState(10);
-
-	const gameInfo = {
-		low: {
-			id: 'low',
-			name: 'Low Risk',
-			min: -25,
-			max: 100
-		},
-		moderate: {
-			id: 'moderate',
-			name: 'Moderate Risk',
-			min: -100,
-			max: 1000
-		},
-		high: {
-			id: 'high',
-			name: 'High Risk',
-			min: -500,
-			max: 2500
-		},
-		severe: {
-			id: 'severe',
-			name: 'Severe Risk',
-			min: -3000,
-			max: 5000
-		}
-	}
+	const [bet, setBet] = useState();
+	const [risk, setRisk] = useState([]);
 
 	const randomizeBet = (min, max) => {
 		return Math.floor(Math.random() * (max - min + 1) + min);
@@ -40,6 +42,8 @@ export default function App() {
 	const handleBet = (e) => {
 		const risk = e.target.id;
 		const bet = randomizeBet(gameInfo[risk].min, gameInfo[risk].max);
+		setBet(bet);
+		setRisk(gameInfo[risk].name);
 
 		if (chance > 0) {
 			setChance(chance - 1);
@@ -48,6 +52,6 @@ export default function App() {
 	}
 
 	return (
-		<Game balance={balance} chance={chance} gameInfo={gameInfo} onBetClick={handleBet} />
+		<Game balance={balance} chance={chance} bet={bet} risk={risk} gameInfo={gameInfo} onBetClick={handleBet} />
 	)
 }

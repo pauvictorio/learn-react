5e80973df8ed18a2d79da86d95f3580e56bd8e02
Paulo Victorio <paulo_victorio@yahoo.com>
2023-04-10 10:11:22 +0800

    Comment out React Strict Mode

--- 14_reaction/src/index.js
+++ 14_reaction/src/index.js
@@ -5,7 +5,9 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<React.StrictMode>
+	// Commented out the StrictMode since it was causes a double render
+	
+	// <React.StrictMode>
 		<App />
-	</React.StrictMode>
+	// </React.StrictMode>
 );

a0854e73f6f3dd1c5ecbcac10e6336393ef428f9
Paulo Victorio <paulo_victorio@yahoo.com>
2023-04-10 11:23:05 +0800

    Comment out StrictMode to avoid calling the API twice

--- 15_image-api/src/index.js
+++ 15_image-api/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'semantic-ui-css/semantic.min.css'
 import './assets/css/index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<React.StrictMode>
+	// Commented out StrictMode to avoid calling the API twice
+	
+	// <React.StrictMode>
 		<App />
-	</React.StrictMode>
+	// </React.StrictMode>
 );

f294524279af882265d43eb2d71631a0eecad3d0
Paulo Victorio <paulo_victorio@yahoo.com>
2023-04-10 10:12:23 +0800

    Add Reaction component

--- 14_reaction/src/components/Reaction.jsx
+++ 14_reaction/src/components/Reaction.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import '../assets/css/Reaction.css'
 
-export default function Welcome() {
+export default function Reaction({ reacts, onReact }) {
+	const handleReacts = (id) => {
+		onReact(id);
+	}
+
 	return (
-		<div>Welcome</div>
+		<div className="Reaction">
+			{
+				reacts.map((react) => (
+					<div className="Reaction__group" key={react.id}>
+						<p>{react.count}</p>
+						<img
+							src={react.src}
+							alt={react.name}
+							onClick={() => handleReacts(react.id)}
+						/>
+					</div>
+				))
+			}
+		</div>
 	)
 }
